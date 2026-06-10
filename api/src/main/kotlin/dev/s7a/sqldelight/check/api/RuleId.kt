3f93a6541dc4122d0a044d6e8e3e919e36d66a89
sya-ri <git@s7a.dev>
2026-06-11 04:30:31 +0900

    Document ID value properties

--- api/src/main/kotlin/dev/s7a/sqldelight/check/api/RuleId.kt
+++ api/src/main/kotlin/dev/s7a/sqldelight/check/api/RuleId.kt
@@ -5,5 +5,6 @@ package dev.s7a.sqldelight.check.api
  */
 @JvmInline
 public value class RuleId(
+    /** Stable rule ID value. */
     public val value: String,
 )
--- api/src/main/kotlin/dev/s7a/sqldelight/check/api/RuleSetId.kt
+++ api/src/main/kotlin/dev/s7a/sqldelight/check/api/RuleSetId.kt
@@ -5,5 +5,6 @@ package dev.s7a.sqldelight.check.api
  */
 @JvmInline
 public value class RuleSetId(
+    /** Stable rule set ID value. */
     public val value: String,
 )
